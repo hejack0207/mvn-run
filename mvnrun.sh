0929915ab54af40a9e7c6259eb45b2ee5d99b869
hejack0207 <hejack0207@sina.com>
2017-07-11 13:56:02 +0800

    get classpath using /dev/stdout

--- mvnrun.sh
+++ mvnrun.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-mvn dependency:build-classpath -Dmdep.outputFile=cp.txt &>/dev/null
-java -cp $(cat cp.txt) -jar target/mvnrun.jar $*
+#mvn dependency:build-classpath -Dmdep.outputFile=cp.txt &>/dev/null
+#java -cp $(cat cp.txt) -jar target/mvnrun.jar $*
+
+java -cp $(mvn -q dependency:build-classpath -Dmdep.outputFile=/dev/stdout) -jar target/mvnrun.jar $*
