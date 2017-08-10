9c6f25dd975c9c550d167dfff581c38c2fa5d27a
hejack0207 <hejack0207@sina.com>
2017-08-10 15:34:12 +0800

    parse command line options using JCommander

--- src/main/java/fr/lteconsulting/mvnrun/MavenRunApp.java
+++ src/main/java/fr/lteconsulting/mvnrun/MavenRunApp.java
@@ -1,5 +1,9 @@
 package fr.lteconsulting.mvnrun;
 
+import java.util.List;
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+
 /**
  * This is the main entry point of the maven run application.
  * 
@@ -8,19 +12,34 @@ package fr.lteconsulting.mvnrun;
  */
 public class MavenRunApp
 {
-	public static void main(String[] args)
-	{
-		if (args == null || args.length < 1)
-		{
-			System.out.println("Usage:\r\njava -jar mvnrun.jar groupId:artifactId:version parameters");
+	@Parameter(names = {"--artifact", "-a"}, description = "like groupId:artifactId:version", required = true)
+	String artifact;
+
+	@Parameter(names = {"--mainclass", "-m"}, description = "main class name, like org.sharpx.cli.Main", required = true)
+	String mainClass;
+
+	@Parameter(description = "arguments to main-class")
+	List<String> arguments;
+
+	@Parameter(names = {"--verbose", "-V"}, description = "verbose")
+	boolean verbose = false;
+
+	@Parameter(names = "--help", help = true)
+	private boolean help;
+
+	public static void main(String[] args) {
+		MavenRunApp cli = new MavenRunApp();
+		JCommander jc = JCommander.newBuilder().addObject(cli).build();
+		jc.parse(args);
+		if (cli.help) {
+			jc.usage();
 			return;
 		}
 
-		String artifact = args[0];
-		String[] arguments = new String[args.length - 1];
-		for (int i = 1; i < args.length; i++)
-			arguments[i - 1] = args[i];
+		cli.run();
+	}
 
-		MavenRun.run(artifact, arguments, null);
+	void run(){
+		MavenRun.run(artifact, arguments.toArray(new String[0]), mainClass);
 	}
 }
